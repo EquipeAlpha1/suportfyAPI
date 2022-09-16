30d3e4834da61c5031efff245d86e8ca0e9134fc
Luis Eduardo <81581633+luisebf01@users.noreply.github.com>
2022-09-16 20:59:17 -0300

    Delete Site directory

--- Site/api.py
+++
@@ -1,6 +0,0 @@
-from app import app
-import os
-
-if __name__=='main':
-    port = int(os.getenv('PORT'), '5000')
-    app.run(host='0.0.0.0', port=port)
--- Site/app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "minha-palavra-secreta"
-
-from app import routes
