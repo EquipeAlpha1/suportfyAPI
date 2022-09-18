329a8852b830d33434ceb343ad9f9f7ea3af48b3
Luis Eduardo <81581633+luisebf01@users.noreply.github.com>
2022-09-17 21:11:07 -0300

    Add files via upload

--- Site/app/routes.py
+++ Site/app/routes.py
@@ -20,55 +20,55 @@ email = Mail(app)
 @app.route('/', methods=['GET','POST'])
 @app.route('/home', methods=['GET','POST'])
 def home():
-    if request.method == 'POST' and request.form.get('mail') != '':
+    if request.method == 'POST':
         name = request.form.get('name')
         mail = request.form.get('mail')
         pc = request.form.get('pc')
-        subject = str(request.form.get('subject'))
-        description = str(request.form.get('description'))
+        subject = request.form.get('subject')
+        description = request.form.get('description')
+        if bool(name) and bool(mail) and bool(pc) and bool(subject) and bool(description):
+            ## Envia um e-mail para o usuário
+            msg = Message('SUPORTE FATEC: Sua solicitação foi recebida!', recipients=[mail])
+            msg.html = "<!DOCTYPE html><html><body>" \
+                        "<div style=""font-family:'Segoe UI', Calibri, Arial, Helvetica; font-size: 14px; max-width: 762px;"">" \
+                        "Fala {}, Beleza?<br /><br />" \
+                        "Seu pedido de suporte foi registrado! E gostaríamos de parabenizá-lo por promover um ambiente FATEC melhor!<br />" \
+                        "O suporte FATEC costuma resolver os problemas em até 3 dias úteis. E até lá, pedimos paciência.<br />" \
+                        "Inclusive, segue abaixo o registro do pedido de suporte realizado.<br /><br />" \
+                        "&emsp;Computador: {}<br />" \
+                        "&emsp;&ensp; &nbsp;Assunto: {}<br />" \
+                        "&emsp;&nbsp; Descrição: {}<br /><br />" \
+                        "Atenciosamente,<br />" \
+                        "GRUPO ALPHA" \
+                        "<br /><br />" \
+                        "<table style='border-top:1px solid #dddddd;border-collapse:collapse' cellpadding='0' cellspacing='0' width='100%'>" \
+                        "<td style='line-height:12px;color:#808080;font-size:10px'>" \
+                        "<b>Este é um e-mail de notificação e foi gerado automaticamente. Por favor, não responda esta mensagem!</b><br />" \
+                        "O Grupo Alpha respeita a sua privacidade e é contra o spam na rede.<br />" \
+                        "<a style='text-decoration:none;color:#808080'>Este e-mail foi enviado para o e-mail [{}] porque este e-mail foi usado para abrir um chamado de suporte na FATEC - SJC.</a><br />" \
+                        "Se você não deseja mais receber esse tipo de e-mail, clique " \
+                        "<a href='https://www.rataalada.com/' style='color: rgb(71, 124, 204); text-decoration: none; display: inline;'>aqui</a>." \
+                        "</table></div></body></html>".format(name, pc, subject, description, mail)
+            email.send(msg)
 
-        ## Enviando Email para usuário
-        msg = Message('SUPORTE FATEC: Sua solicitação foi recebida!', recipients=[mail])
-        msg.html = "<!DOCTYPE html><html><body>" \
+            ## Envia um e-mail para a equipe de suporte
+            msg = Message('SUPORTE FATEC: Chamado de manutenção recebido!', recipients=['api.ads.2022@gmail.com'])
+            msg.html = "<!DOCTYPE html><html><body>" \
                     "<div style=""font-family:'Segoe UI', Calibri, Arial, Helvetica; font-size: 14px; max-width: 762px;"">" \
-                    "Fala {}, Beleza?<br /><br />" \
-                    "Seu pedido de suporte foi registrado! E gostaríamos de parabenizá-lo por promover um ambiente FATEC melhor!<br />" \
-                    "O suporte FATEC costuma resolver os problemas em até 3 dias úteis. E até lá, pedimos paciência.<br />" \
-                    "Inclusive, segue abaixo o registro do pedido de suporte realizado.<br /><br />" \
-                    "&emsp;Computador: {}<br />" \
-                    "&emsp;&ensp; &nbsp;Assunto: {}<br />" \
-                    "&emsp;&nbsp; Descrição: {}<br /><br />" \
-                    "Atenciosamente,<br />" \
-                    "GRUPO ALPHA" \
-                    "<br /><br />" \
-                    "<table style='border-top:1px solid #dddddd;border-collapse:collapse' cellpadding='0' cellspacing='0' width='100%'>" \
-                    "<td style='line-height:12px;color:#808080;font-size:10px'>" \
-                    "<b>Este é um e-mail de notificação e foi gerado automaticamente. Por favor, não responda esta mensagem!</b><br />" \
-                    "O Grupo Alpha respeita a sua privacidade e é contra o spam na rede.<br />" \
-                    "<a style='text-decoration:none;color:#808080'>Este e-mail foi enviado para o e-mail [{}] porque este e-mail foi usado para abrir um chamado de suporte na FATEC - SJC.</a><br />" \
-                    "Se você não deseja mais receber esse tipo de e-mail, clique " \
-                    "<a href='https://www.rataalada.com/' style='color: rgb(71, 124, 204); text-decoration: none; display: inline;'>aqui</a>." \
-                    "</table></div></body></html>".format(name, pc, subject, description, mail)
-        email.send(msg)
-
-        ##Enviando Email para Suporte
-        msg = Message('SUPORTE FATEC: Chamado de manutenção recebido!', recipients=['api.ads.2022@gmail.com'])
-        msg.html = "<!DOCTYPE html><html><body>" \
-                   "<div style=""font-family:'Segoe UI', Calibri, Arial, Helvetica; font-size: 14px; max-width: 762px;"">" \
-                    "Fala Suporte Alpha, Beleza?<br /><br />" \
-                    "Foi registrado um pedido de manutenção para um computador com problema na sala 404.<br />" \
-                    "Segue abaixo as informações do chamado:<br /><br />" \
-                    "  Solicitante: {}<br />"\
-                    "         Sala: 404<br />"\
-                    "   Computador: {}<br />" \
-                    "      Assunto: {}<br />" \
-                    "    Descrição: {}<br /><br />" \
-                    "Atenciosamente,<br />" \
-                    "GRUPO ALPHA" \
-                    "<br /><br />" \
-                    "<table style='border-top:1px solid #dddddd;border-collapse:collapse' cellpadding='0' cellspacing='0' width='100%'>" \
-                    "<b>Este é um e-mail de notificação e foi gerado automaticamente. Por favor, não responda esta mensagem!</b><br />" \
-                    "<a style='text-decoration:none;color:#808080'>Este e-mail foi enviado para o e-mail [api.ads.2022@gmail.com] porque este e-mail foi registrado para a equipe de suporte na FATEC - SJC.</a><br />" \
-                    "</table></div></body></html>".format(name, pc, subject, description)
-        email.send(msg)
+                        "Fala Suporte Alpha, Beleza?<br /><br />" \
+                        "Foi registrado um pedido de manutenção para um computador com problema na sala 404.<br />" \
+                        "Segue abaixo as informações do chamado:<br /><br />" \
+                        "  Solicitante: {}<br />"\
+                        "         Sala: 404<br />"\
+                        "   Computador: {}<br />" \
+                        "      Assunto: {}<br />" \
+                        "    Descrição: {}<br /><br />" \
+                        "Atenciosamente,<br />" \
+                        "GRUPO ALPHA" \
+                        "<br /><br />" \
+                        "<table style='border-top:1px solid #dddddd;border-collapse:collapse' cellpadding='0' cellspacing='0' width='100%'>" \
+                        "<b>Este é um e-mail de notificação e foi gerado automaticamente. Por favor, não responda esta mensagem!</b><br />" \
+                        "<a style='text-decoration:none;color:#808080'>Este e-mail foi enviado para o e-mail [api.ads.2022@gmail.com] porque este e-mail foi registrado para a equipe de suporte na FATEC - SJC.</a><br />" \
+                        "</table></div></body></html>".format(name, pc, subject, description)
+            email.send(msg)
     return render_template('content.html')
--- Site/app/static/js/jQuery.js
+++ Site/app/static/js/jQuery.js
@@ -1,17 +1,21 @@
 $(document).on('submit','#formEmail',function(e)
-    {
+  {
+    var btn = ($(document.activeElement).val());
+    if (btn == 'SendMail') {
       e.preventDefault();
-      $.ajax(
-      {
+      $.ajax
+      ({
         type:'POST',
         url:'/', /* URL para rota flask */
-        data: /* a função flask/python vai puxar os dados desse 'dicionário' */
+        data: /* A função python vai puxar os dados desse 'dicionário' */
         {
-          name:$("#nome").val(), /* na onde tem # vai o ID do objeto HTML que está dentro da tag 'form' */
+          name:$("#nome").val(), /* Na onde tem # vai o ID do objeto HTML que está dentro do 'formEmail' */
           mail:$("#mail").val(),
           pc:$("input[name='computador']:checked").val(),
           subject:$("#exampleDataList").val(),
           description:$("#exampleFormControlTextarea1").val()          
         }
       });
-    });
+      $('#formEmail')[0].reset(); /* Reseta o formulário para não enviar e-mail atoa */
+    }    
+  });
