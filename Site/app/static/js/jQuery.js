8fa17f57050186737ed8588658174547b1e687a0
Luis Eduardo <luisebf01@gmail.com>
2022-10-29 18:09:13 -0300

    Update jQuery.js

--- Site/app/static/js/jQuery.js
+++ Site/app/static/js/jQuery.js
@@ -4,39 +4,21 @@ $(document).on('submit','#formEmail',function(e) {/* <!-- Essa função será at
     var c1_email = $("#c1_email").val();
     var c1_floor = $("#c1_floor").val();
     var c1_room = $("#c1_room").val();
-    var c2_pc = $("input[name='c2_computadores']:checked").val();
+    var c2_pc = $("input[name='c2_computadores']:checked").val(); /* Pega o valor(número) do computador que está selecionado */
     var c3_assunto = $("#c3_subject").val();
     var c3_texto = $("#c3_description").val();
 
-    var btn = ($(document.activeElement).val()); /* Salva o valor do botão, para verificar */
+    var btn = ($(document.activeElement).val()); /* Salva o valor do botão 'Submit', para verificar */
 
     if (btn == 'SendMail') {
-
-        if (!c1_name && !c1_email && !c1_floor && !c1_room && !c2_pc && !c3_assunto && !c3_texto) {      
-            return;
+        
+        if (!c1_name && !c1_email && !c1_floor && !c1_room && !c2_pc && !c3_assunto && !c3_texto) {    
+            return; /* Caso falte preencher algum campo, sai do script */
         };
 
         e.preventDefault(); /* Essa função evita de recarregar a página */
 
-        var nFile = document.getElementById('formFile').files.length;    
-
-        if (nFile > 0) { /* Verifica se foi inserido um arquivo na solicitação */
-
-            var form_data = new FormData();           
-            form_data.append('file', document.getElementById('formFile').files[0]); 
-
-            $.ajax ({
-
-                type:'POST',
-                url:'/upload_file', /* URL para rota flask */
-                data: form_data, /* Arquivo do formulário para o Flask fazer upload */
-                contentType: false,
-                cache: false,
-                processData: false  
-                              
-            });
-
-        };
+        var nFile = document.getElementById('formFile').files.length; /* Salva o número de arquivos anexados ao formulário */
 
         $.ajax ({
 
@@ -51,6 +33,26 @@ $(document).on('submit','#formEmail',function(e) {/* <!-- Essa função será at
                 subject : c3_assunto,
                 description : c3_texto
             }
+
+        }).done(function(data) {
+
+            if (nFile > 0) { /* Verifica se foi inserido pelo menos um arquivo na solicitação */
+
+                var form_data = new FormData();           
+                form_data.append('file', document.getElementById('formFile').files[0]); /* Anexa o arquivo ao objeto FormData */
+
+                $.ajax ({
+
+                    type:'POST',
+                    url:'/upload_file', /* URL para rota flask */
+                    data: form_data, /* Objeto com o arquivo do formulário para o Flask fazer upload */
+                    contentType: false,
+                    cache: false,
+                    processData: false  
+                                
+                });
+
+            };
 
         });
 
