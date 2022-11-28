1454b0b7519cd460a92eb4fbbde9a02813493433
Luis Eduardo <luisebf01@gmail.com>
2022-11-28 17:27:06 -0300

    Revert "Merge branch 'Sprint-4-Nilber' into sprint-4-LUIS"
    This reverts commit 968fe22d06d665a4eb6574722674f84584e264ec, reversing
    changes made to 3989ef70eb0542c3185adf41f1d59415621cb3a2.

--- Site/app/routes.py
+++ Site/app/routes.py
@@ -164,27 +164,27 @@ def create_request():
         except:
             return "<h2>ERRO: O servidor está fora do ar ou você inseriu no formulário algum dado inválido. Sua solicitação não foi registrada.</h2>"
 
-        # ## Envia um e-mail de notificação para a equipe de suporte
-        # msg = Message('SUPORTE FATEC: Chamado de manutenção recebido!', recipients=['luisebf01@gmail.com'])
-        # msg.html = "<!DOCTYPE html><html><body>" \
-        #         "<div style=""font-family:'Segoe UI', Calibri, Arial, Helvetica; font-size: 14px; max-width: 762px;"">" \
-        #             "Fala Suporte Alpha, Beleza?<br /><br />" \
-        #             "Acabaram de abrir uma solicitação de suporte, e as informações seguem abaixo:<br /><br />" \
-        #             "  Solicitante: {}<br />" \
-        #             "       E-mail: {}<br />" \
-        #             "         Piso: {}<br />" \
-        #             "         Sala: {}<br />" \
-        #             "   Computador: {}<br />" \
-        #             "      Assunto: {}<br />" \
-        #             "    Descrição: {}<br /><br />" \
-        #             "Atenciosamente,<br />" \
-        #             "GRUPO ALPHA" \
-        #             "<br /><br />" \
-        #             "<table style='border-top:1px solid #dddddd;border-collapse:collapse' cellpadding='0' cellspacing='0' width='100%'>" \
-        #             "<td style='line-height:12px;color:#808080;font-size:10px'>" \
-        #             "<b>Este é um e-mail de notificação e foi gerado automaticamente. Por favor, não responda esta mensagem!</b><br />" \
-        #             "<a style='text-decoration:none;color:#808080'>Este e-mail foi enviado para o e-mail [api.ads.2022@gmail.com] porque este e-mail foi registrado para a equipe de suporte na FATEC - SJC.</a><br />" \
-        #             "</table></div></body></html>".format(name, mail, floor, room, pc, subject, description)
+        ## Envia um e-mail de notificação para a equipe de suporte
+        msg = Message('SUPORTE FATEC: Chamado de manutenção recebido!', recipients=['luisebf01@gmail.com'])
+        msg.html = "<!DOCTYPE html><html><body>" \
+                "<div style=""font-family:'Segoe UI', Calibri, Arial, Helvetica; font-size: 14px; max-width: 762px;"">" \
+                    "Fala Suporte Alpha, Beleza?<br /><br />" \
+                    "Acabaram de abrir uma solicitação de suporte, e as informações seguem abaixo:<br /><br />" \
+                    "  Solicitante: {}<br />" \
+                    "       E-mail: {}<br />" \
+                    "         Piso: {}<br />" \
+                    "         Sala: {}<br />" \
+                    "   Computador: {}<br />" \
+                    "      Assunto: {}<br />" \
+                    "    Descrição: {}<br /><br />" \
+                    "Atenciosamente,<br />" \
+                    "GRUPO ALPHA" \
+                    "<br /><br />" \
+                    "<table style='border-top:1px solid #dddddd;border-collapse:collapse' cellpadding='0' cellspacing='0' width='100%'>" \
+                    "<td style='line-height:12px;color:#808080;font-size:10px'>" \
+                    "<b>Este é um e-mail de notificação e foi gerado automaticamente. Por favor, não responda esta mensagem!</b><br />" \
+                    "<a style='text-decoration:none;color:#808080'>Este e-mail foi enviado para o e-mail [api.ads.2022@gmail.com] porque este e-mail foi registrado para a equipe de suporte na FATEC - SJC.</a><br />" \
+                    "</table></div></body></html>".format(name, mail, floor, room, pc, subject, description)
 
         ## Se houver uma imagem na solicitação, anexa no e-mail para a equipe de suporte
         global tempFilename, tempEvent
